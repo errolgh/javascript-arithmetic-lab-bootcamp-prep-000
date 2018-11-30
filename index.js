db1fec01e7c84074256586a592c1c8ba1e346dfd
Errol W <ewatsonrei@gmail.com>
2018-11-30 19:00:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+ function add(a, b) {
+   return (a + b)
+ }
+ 
+ add()
+ 
+ function subtract(a, b) {
+   return (a - b)
+ }
+ 
+ subtract()
+ 
+ var a = 5
+ var b = 6
+ 
+ function multiply(a, b) {
+   return (a * b);
+ }
+ 
+ multiply()
+ 
+ function divide(a, b) {
+   return (a / b);
+ }
+ 
+ divide()
+ 
+inc()
+
+
+// "dnc(n) decrements n and returns the result:" function
+ 
+ 
+ 
+// space
+
+function preserveDecimal(n) {
+  return('nonsense' + 1)
+}
+
+preserveDecimal()
+
+
+
