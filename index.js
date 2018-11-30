9ed656027dc3b821a9121bc7cd40d0c40b3b4079
Errol W <ewatsonrei@gmail.com>
2018-11-30 23:54:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+ function add(a, b) {
+   return (a + b)
+ }
+ 
+ add()
+ 
+ function subtract(a, b) {
+   return (a - b)
+ }
+ 
+ subtract()
+ 
+ var a = 5
+ var b = 6
+ 
+ function multiply(a, b) {
+   return (a * b);
+ }
+ 
+ multiply()
+ 
+ function divide(a, b) {
+   return (a / b);
+ }
+ 
+ divide()
+ 
+inc()
+
+
+function preserveDecimal(n) {
+  return('nonsense' + 1)
+}
+
+var n = 7
+
+
+
+
+
+
